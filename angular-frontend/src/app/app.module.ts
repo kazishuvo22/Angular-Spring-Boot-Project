72592f9159971bb9b343532415f70fa6cc4c814c
kazishuvo22 <kazishuvomain@gmail.com>
2020-12-22 14:28:24 +0600

    Angular for mongodb

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TutorialDetailsComponent } from './components/tutorial/tutorial-details/tutorial-details.component';
+import { CreateTutorialComponent } from './components/tutorial/create-tutorial/create-tutorial.component';
+import { UpdateTutorialComponent } from './components/tutorial/update-tutorial/update-tutorial.component';
+import { TutorialListComponent } from './components/tutorial/tutorial-list/tutorial-list.component';
+import {FormsModule} from '@angular/forms';
+import { AddDoctorComponent } from './doctor/add-doctor/add-doctor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TutorialDetailsComponent,
+    CreateTutorialComponent,
+    UpdateTutorialComponent,
+    TutorialListComponent,
+    AddDoctorComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        RouterModule,
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ angular-frontend/src/app/components/tutorial/tutorial-list/tutorial-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TutorialService } from '../../../services/tutorial.service';
+import { Tutorial } from '../../../classes/tutorial';
+
+@Component({
+  selector: 'app-tutorial-list',
+  templateUrl: './tutorial-list.component.html',
+  styleUrls: ['./tutorial-list.component.css']
+})
+export class TutorialListComponent implements OnInit {
+  tutorials: Observable<Tutorial[]>;
+
+  constructor(private tutorialService: TutorialService, private router: Router) { }
+
+  ngOnInit(): void {
+    console.log('Tutorial list');
+    this.reloadData();
+  }
+
+  reloadData(){
+    this.tutorials = this.tutorialService.getAll();
+    console.log(this.tutorials);
+  }
+
+  deleteTutorial(id: string) {
+    this.tutorialService.delete(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+
+  tutorialDetails(id: string){
+    this.router.navigate(['details', id]);
+  }
+
+  updateTutorial(id: string){
+    this.router.navigate(['update', id]);
+  }
+}
